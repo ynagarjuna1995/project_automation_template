5339e33762ba835436914d8cc03c9b458e485800
Nagarjuna <ynagarjuna2012@gmail.com>
2014-12-25 02:42:53 +0530

    gruntfile modified to load the plugins at a time

--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,16 @@
 module.exports = function(grunt){
 
-	//load jshint plugin
-	grunt.loadNpmTasks('grunt-contrib-jshint');
+
+Object.keys(require('./package.json').devDependencies).forEach(function(devDep) {
+  if(devDep.substring(0,6) == "grunt-") {
+    grunt.loadNpmTasks(dep);
+  }
+});
+
+
+
+	 // Load all grunt tasks
+     // Find all of the task which start with `grunt-` and load them, rather than explicitly declaring them all
+     // require('matchdep').filterDev(['grunt-*', '!grunt-cli']).forEach(grunt.loadNpmTasks);
+     //matchdep is a plugin - Install through node package manager 
 };
