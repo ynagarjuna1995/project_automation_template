9f37ae573ecd8d5a791a1fc452ca86c5c0a99480
Nagarjuna <ynagarjuna2012@gmail.com>
2014-12-27 23:32:48 +0530

    after fxing syataxin gruntfile and adding client side jshint rules

--- gruntfile.js
+++ gruntfile.js
@@ -1,17 +1,48 @@
 module.exports = function(grunt){
 
+	pkg: grunt.file.readJSON('package.json'),
+
 grunt.initConfig({
-	 jscs: {
-            
+
+	// ### grunt-contrib-jshint
+	// Linting rules, run as part of `grunt validate`. See [grunt validate](#validate) and its subtasks for
+	// more information.
+	jshint :{
+
+				client: {
+						src: ['gruntfile.js','src/js/client/**/.js'], // Specify Paths for js files for client side part in the array
+						options: {
+							config: '.client_jshintrc'
+						}
+				}
+				server: {
+						src: ['src/js/server/**/.js'], //Specify paths for js files for server side (nodejs) in the array
+						options:{
+							config: '.jshintrc'
+						}
+				},
+	},
+	// ### grunt-jscs
+	// Code style rules, run as part of `grunt validate`. See [grunt validate](#validate) and its subtasks for
+	// more information.
+	 jscs :{
+
         client: {
-            src: ['gruntfile.js', 'src/js/*.js'],
+            src: ['gruntfile.js', 'src/js/client/**/*.js'],
             options: {
              config: '.jscsrc'
+					},
+				},
+				server: {
+					//	main: index.js,  // main is optional specify your server side javascript nodejs files
+						src: ['src/js/server/**/.js'],
+						options:{
+							config: '.jscsrc'
+					}
 
-            }
-        }
+				}
+	}			
         // You can add more configurations over here
-      }
 })
 
 Object.keys(require('./package.json').devDependencies).forEach(function(devDep) {
@@ -24,8 +55,8 @@ Object.keys(require('./package.json').devDependencies).forEach(function(devDep) 
 // Load all grunt tasks
 // Find all of the task which start with `grunt-` and load them, rather than explicitly declaring them all
 // require('matchdep').filterDev(['grunt-*', '!grunt-cli']).forEach(grunt.loadNpmTasks);
-//matchdep is a plugin - Install through node package manager 
+//matchdep is a plugin - Install through node package manager
 
- grunt.registerTask('default', ['jscs']);
- // Check documentation http://gruntjs.com/api/grunt.task 
+ grunt.registerTask('default', ['jshint','jscs']);
+ // Check documentation http://gruntjs.com/api/grunt.task
 };
